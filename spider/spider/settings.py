a9af632134ac45d787a0198c515101ba2af4232a
rieseflo <rieseflo@students.zhaw.ch>
2024-03-29 17:03:30 +0100

    commented out robots.txt

--- spider/spider/settings.py
+++ spider/spider/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "spider.spiders"
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/97.0'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
